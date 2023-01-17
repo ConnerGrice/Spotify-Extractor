e8aa419cb168d1c4d094acb4a9cd70cc4475e32e
Conner Grice <connergrice12@gmail.com>
2023-01-17 11:44:10 +0000

    Converts items into dataclasses

--- classes/Items.py
+++ classes/Items.py
@@ -1,37 +1,39 @@
+from dataclasses import dataclass
+from typing import List
 
+@dataclass
 class PlaylistItem:
-    """A container for relavent playlist information"""
-    def __init__(self,playlist:dict) -> None:
-        self.id = playlist['id']
-        self.name = playlist['name']
-        self.owner = playlist['owner']['display_name']
-        self.length = playlist['tracks']['total']
-        self.version = playlist['snapshot_id']
+    """Container for playlist information"""
+    id:str
+    name:str
+    owner:str
+    length:int
+    version:str
 
+@dataclass
 class TrackItem:
     """A container for relavent track information"""
-    def __init__(self,track:dict,track_extra:dict,playlist_id:str) -> None:
-        self.id = track['track']['id']
-        self.name = track['track']['name']
-        self.duration = track['track']['duration_ms']
-        self.dance = track_extra[0]['danceability']
-        self.tempo = track_extra[0]['tempo']
-        self.energy = track_extra[0]['energy']
-        self.artist_id = track['track']['artists'][0]['id']
-        self.album_id = track['track']['album']['id']
-        self.playlist_id = playlist_id
-        
+    id:str
+    name:str
+    duration:int
+    dance:float
+    tempo:float
+    energy:float
+    artist_id:str
+    album_id:str
+    playlist_id:str
+
+@dataclass
 class AlbumItem:
     """A container for relavent album information"""
-    def __init__(self,album:dict,artist_id:str) -> None:
-        self.id = album['id']
-        self.name = album['name']
-        self.release_date = album['release_date']
-        self.artist_id = artist_id
+    id:str
+    name:str
+    release_date:str
+    artist_id:str
 
+@dataclass
 class ArtistItem:
     """A container for relavent artist information"""
-    def __init__(self,artist:dict) -> None:
-        self.id = artist['id']
-        self.name = artist['name']
-        self.genre = artist['genres']
+    id:str
+    name:str
+    genre:List[str]
--- classes/SpotifyAPI.py
+++ classes/SpotifyAPI.py
@@ -10,24 +10,41 @@ class SpotifyAPI():
     """Controls functionality for spotipy"""
 
     def __init__(self) -> None:
+        #Gets info for auth manager
         self.ID = cred.clientID
         self.SECRET = cred.clientSecret
         self.URI = cred.redirectURL
         self.SCOPE = "playlist-read-private user-read-private"
 
+        #Generates manager
         self.auth_manager = SpotifyOAuth(
             client_id=self.ID,
             client_secret=self.SECRET,
             redirect_uri=self.URI,
             scope=self.SCOPE)
 
+        #Establishes connection to Spotify
         self.sp = spotipy.Spotify(auth_manager=self.auth_manager)
 
     def get_playlists(self) -> List[Items.PlaylistItem]:
         """Gets the playlist items"""
         
+        #Gets list of playlists
         playlist_list = self.sp.current_user_playlists()['items']
-        return [Items.PlaylistItem(playlist) for playlist in playlist_list]
+
+        output = []
+        #Loops through playlists and extracts wanted information
+        for playlist in playlist_list:
+            id = playlist['id']
+            name = playlist['name']
+            owner = playlist['owner']['display_name']
+            length = playlist['tracks']['total']
+            version = playlist['snapshot_id']
+
+            #Puts info into playlist dataclass
+            output.append(Items.PlaylistItem(id,name,owner,length,version))
+
+        return output
 
     def get_tracks(self,playlist:Items.PlaylistItem) -> List[Items.TrackItem]:
         """Gets all tracks in a playlist"""
@@ -48,24 +65,65 @@ class SpotifyAPI():
         for i,track in enumerate(tracks):
             print(f"Processing Track {i+1}/{len(tracks)}")
 
+            #Gets extra audio data
             track_features = self.sp.audio_features(track['track']['id'])
-            track_items.append(Items.TrackItem(track,track_features,playlist.id))
+
+            id = track['track']['id']
+            name = track['track']['name']
+            duration = track['track']['duration_ms']
+            dance = track_features[0]['danceability']
+            tempo = track_features[0]['tempo']
+            energy = track_features[0]['energy']
+            artist_id = track['track']['artists'][0]['id']
+            album_id = track['track']['album']['id']
+
+            track_item = Items.TrackItem(
+                id,
+                name,
+                duration,
+                dance,
+                tempo,
+                energy,
+                artist_id,
+                album_id,
+                playlist.id
+                )
+
+            track_items.append(track_item)
         
         return track_items
     
     def get_albums(self,track_list:List[Items.TrackItem]) -> List[Items.AlbumItem]:
         """Gets the albums from a list of tracks"""
         albums = []
+
+        #Loops through track list and gets corresponding album
         for i,track in enumerate(track_list):
             print(f"Processing album {i+1}/{len(track_list)}")
             album = self.sp.album(track.album_id)
-            albums.append(Items.AlbumItem(album,track.artist_id))
+
+            id = album['id']
+            name = album['name']
+            release_date = album['release_date']
+            artist_id = track.artist_id
+            
+            album_item = Items.AlbumItem(id,name,release_date,artist_id)
+            albums.append(album_item)
         return albums
 
-    def get_artist(self,track_list:List[Items.TrackItem]) -> List[Items.ArtistItem]:
+    def get_artists(self,track_list:List[Items.TrackItem]) -> List[Items.ArtistItem]:
         """Gets the artist from a list of tracks"""
         artists = []
-        for track in track_list:
+
+        #Loops through track list and gets corresponding artist
+        for i,track in enumerate(track_list):
+            print(f"Processing artist {i+1}/{len(track_list)}")
             artist = self.sp.artist(track.artist_id)
-            artists.append(Items.ArtistItem(artist))
+
+            id = artist['id']
+            name = artist['name']
+            genres = artist['genres']
+
+            artist_item = Items.ArtistItem(id,name,genres)
+            artists.append(artist_item)
         return artists
