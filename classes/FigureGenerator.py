e9aa028c7f4642ceebf2e91453cd61b6ead61888
Conner Grice <connergrice12@gmail.com>
2023-02-03 17:17:15 +0000

    Fixing bug to allow for filtering to work for all playlists

--- classes/FigureGenerator.py
+++ classes/FigureGenerator.py
@@ -34,6 +34,7 @@ class FigureGenerator:
 
         hover = HoverTool(tooltips=[
             ("Name","@Name"),
+            ("Artist","@Artist"),
             ("Playlist", "@PlaylistID")
         ])
         
@@ -43,36 +44,42 @@ class FigureGenerator:
         
         #Defining the data to be used in the plot
         source = ColumnDataSource(data)
-        filtered = ColumnDataSource(data)
+        filtered = ColumnDataSource(dict(
+            Dance=[],
+            Energy=[],
+            Playlist_activeID=[],
+            PlaylistID=[],
+
+            ))
 
         p = figure(title= "Dance vs Energy",x_axis_label="Dance",y_axis_label="Energy",tools=[hover])
         p.scatter(x="Dance",y="Energy",source=filtered)
 
-        check_boxes = CheckboxGroup(labels=labels,active=list(range(len(labels))))
+        check_boxes = CheckboxGroup(labels=labels,active=[])
 
         callback = CustomJS(args=dict(source=source,filtered=filtered),code="""
             var data = source.data;
             var f_data = filtered.data;
-            var new_index = [];
             var new_dance = [];
             var new_energy = [];
             var new_label = [];
+            var new_playlist = [];
 
             const active = cb_obj.active;
             
-            var index = data['index'];
             var dance = data['Dance'];
             var energy = data['Energy'];
             var labels = data['Playlist_activeID'];
+            var playlist = data['PlaylistID']
 
             for (let i=0;i<dance.length;i++){
                 for (let j=0;j<active.length;j++){
                     if (labels[i] == active[j]){
-                        console.log(labels[i])
-                        new_index.push(index[i]);
                         new_dance.push(dance[i]);
                         new_energy.push(energy[i]);
-                        new_label.push(labels[i])
+                        new_label.push(labels[i]);
+                        new_playlist.push(playlist[i]);
+
                     }
                 }
             }   
@@ -80,7 +87,7 @@ class FigureGenerator:
             f_data['Dance'] = new_dance;
             f_data['Energy'] = new_energy;
             f_data['Playlist_activeID'] = new_label;
-            f_data['index'] = new_index
+            f_data['PlaylistID'] = new_playlist;
 
             filtered.change.emit();
         """)
